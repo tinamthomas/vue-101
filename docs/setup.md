6034c46b68c5b15ca97b6f0cfef007fbda81e378
Tina Thomas <tinamariathomas@gmail.com>
2024-05-01 15:34:02 -0500

    Adding tailwind css dependency

--- docs/setup.md
+++ docs/setup.md
@@ -3,3 +3,13 @@
 ```
 npm init vue@3
 ```
+
+### adding tailwind CSS
+
+```
+npm install --save-dev tailwindcss po
+stcss autoprefixer
+
+npx tailwindcss init -p
+
+```
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: []
+}
