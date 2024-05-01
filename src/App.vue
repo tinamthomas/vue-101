945f1c2e5f443ab89857048fcf08156d67934b09
Tina Thomas <tinamariathomas@gmail.com>
2024-05-01 11:08:10 -0500

    Deleting files and going back to simple app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="wrapper">
-    <h1>Welcome!</h1>
-    <HelloWorld msg="You did it!" />
-  </div>
+  <h1>Hello Vue</h1>
 </template>
-
-<style scoped>
-h1 {
-  color: purple;
-}
-</style>
