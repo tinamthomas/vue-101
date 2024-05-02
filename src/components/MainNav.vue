7c5f21965457f1922911b8c89f3a268caa8be8e2
Tina Thomas <tinamariathomas@gmail.com>
2024-05-02 11:50:29 -0500

    basic vitest

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,6 +3,9 @@
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
         <a href="/ " class="flex h-full items-center text-xl">{{ company }}</a>
+        <h2 class="ml-8 flex h-full items-center">
+          Developed by {{ author.firstName }} {{ author.lastName }}
+        </h2>
       </div>
     </div>
   </header>
@@ -13,7 +16,8 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Some careers'
+      company: 'Some careers',
+      author: { firstName: 'Tina', lastName: 'Thomas' }
     }
   }
 }
