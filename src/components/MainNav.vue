b5f8c80eec760f16e5d19ffb288c1ab62e92882a
Tina Thomas <tinamariathomas@gmail.com>
2024-05-02 11:18:06 -0500

    Send data to component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
-        <a href="/ " class="flex h-full items-center text-xl">Careers</a>
+        <a href="/ " class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -10,6 +10,11 @@
 
 <script>
 export default {
-  name: 'MainNav'
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Some careers'
+    }
+  }
 }
 </script>
