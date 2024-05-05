fea69d4cdfe80fe411890096f5408d86c63a0b99
Tina Thomas <tinamariathomas@gmail.com>
2024-05-05 18:00:33 -0500

    Box shadow on sign in

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ export default {
       colors: {
         'brand-gray-1': '#dadce0',
         'brand-blue-1': '#1967d2'
+      },
+      boxShadow: {
+        blue: '0 0 3px 3px #4285f4'
       }
     }
   },
