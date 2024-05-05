39b887aa2017d1eee0b00400c4c48b33651adb8f
Tina Thomas <tinamariathomas@gmail.com>
2024-05-05 08:18:29 -0500

    Adding nav items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,6 +3,22 @@
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="h-full">
+              <a href="" class="flex h-full items-center py-2.5">Teams</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Location</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">About us</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Contact us</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
